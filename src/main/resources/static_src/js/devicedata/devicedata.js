380bef43a7b59e8666b45b52f7e52c0c984f6915
warhog <547310+warhog@users.noreply.github.com>
2018-12-19 11:41:31 +0100

    reorder items, add cell voltage

--- src/main/resources/static_src/js/devicedata/devicedata.js
+++ src/main/resources/static_src/js/devicedata/devicedata.js
@@ -1,12 +1,13 @@
 /* global moment */
 angular.module('devicedata', ['ngDialog', 'ngProgress', 'ui.bootstrap']).controller('devicedata', function (
-        $scope, ngDialog, $location, ngProgressFactory, DeviceDataService, Alerts, LoginService
+        $scope, ngDialog, $location, ngProgressFactory, DeviceDataService, Alerts
         ) {
 
     $scope.chipid = parseInt($location.search().chipid);
     $scope.alerts = Alerts;
 
     $scope.isLoading = false;
+    $scope.cells = 1;
 
     $scope.progressbar = ngProgressFactory.createInstance();
     console.log("chipid", $scope.chipid);
@@ -20,10 +21,22 @@ angular.module('devicedata', ['ngDialog', 'ngProgress', 'ui.bootstrap']).control
             DeviceDataService.loadDeviceData($scope.chipid)
                     .then(function (data) {
                         $scope.data = data;
-                        console.log($scope.data);
+                        console.log("data", $scope.data);
+                        if ($scope.data.voltage > 7 && $scope.data.voltage <= 10) {
+                            $scope.cells = 2;
+                        } else if ($scope.data.voltage > 10 && $scope.data.voltage <= 13) {
+                            $scope.cells = 3;
+                        } else if ($scope.data.voltage > 13 && $scope.data.voltage <= 17) {
+                            $scope.cells = 4;
+                        } else if ($scope.data.voltage > 17 && $scope.data.voltage <= 21.5) {
+                            $scope.cells = 5;
+                        } else if ($scope.data.voltage > 21.5 && $scope.data.voltage <= 26) {
+                            $scope.cells = 6;
+                        }
+                        console.log("cells", $scope.cells);
                     })
                     .catch(function (response) {
-                        if (response.data === null) {
+                        if (!response.data || !response.data.message) {
                             response.data = {message: "unable to load"};
                         }
                         ngDialog.open({template: "dataFailure", scope: $scope, data: {message: "cannot load device data: " + response.data.message}});
