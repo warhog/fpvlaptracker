a05596e2f4f0abbec89e96b3d93d12ce85389b38
warhog <547310+warhog@users.noreply.github.com>
2018-12-19 20:52:10 +0100

    improve release builder

--- build_release.sh
+++ build_release.sh
@@ -7,7 +7,6 @@ if [ $# -eq 0 ]; then
 fi
 
 VERSION=$1
-
 CURRENT_BRANCH=`git branch | grep \* | cut -d ' ' -f2`
 START_PATH=`pwd`
 
@@ -21,13 +20,15 @@ echo "build release version ${VERSION}"
 
 gradle -Pversion=$1 build
 
-mkdir build/release
-cp build/libs/fpvlaptracker-${VERSION}.jar build/release/
-APPPROPS=build/release/application.properties
+mkdir -p build/release
+cd build/release
+rm -fr *
+
+cp ../libs/fpvlaptracker-${VERSION}.jar ./
+APPPROPS=application.properties
 touch ${APPPROPS}
 echo "server.port: 80" > ${APPPROPS}
 
-cd build/release
 zip -9 -r fpvlaptracker-${VERSION}.zip *
 cp fpvlaptracker-${VERSION}.zip ${START_PATH}/fpvlaptracker-${VERSION}.zip
 cd ${START_PATH}
